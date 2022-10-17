c4b4ecb691a9d86998eca570969bd6a93071f808
齋藤渉 <saitouwataru@saitouwatarunoMacBook-Pro.local>
2022-10-17 17:25:58 +0900

    JavaScriptを学ぼう 課題02

---
+++ app_02.js
@@ -0,0 +1,13 @@
+let num = 1;
+for (let i = 1; i < 51; i++)  {
+    console.log(i);
+     if((i % 4) == 0) {
+     console.log('4で割れる数です。' + i + '');
+ }
+ if(i == 50) {
+    alert('50回カウントが終わりました。');
+　}
+if(i == 50) {
+    console.log('50回カウントが終わりました。');
+　}
+}
